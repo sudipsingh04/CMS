047e0998e57246f890802ee3cb73a0513625b086
sudipsingh04 <sudipsingh04@gmail.com>
2020-08-08 23:07:13 +0530

    Store post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\Posts\CreatePostRequest;
 
 class PostsController extends Controller
 {
@@ -32,9 +34,21 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostRequest $request)
     {
-        //
+        $image = $request->image->store('posts');
+
+        Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'published_at' => $request->published_at,
+            'image' => $image,
+        ]);
+
+        session()->flash('success', 'Post created successfully');
+
+        return redirect(route('posts.index'));
     }
 
     /**
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -17,7 +17,7 @@
                 </ul>
             </div>
         @endif
-        <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store')}}" method="post">
+        <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store')}}" method="post" enctype="multipart/form-data">
             @csrf
             @if (isset($post))
                 @method('put')
