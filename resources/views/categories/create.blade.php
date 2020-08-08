498de03cb42a1311685b9de297c90c0305135815
sudipsingh04 <sudipsingh04@gmail.com>
2020-08-08 15:28:34 +0530

    Create category

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -4,7 +4,19 @@
 <div class="card card-default">
     <div class="card-header">Create Category</div>
     <div class="card-body">
-        <form action="" method="post">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="list-group">
+                    @foreach ($errors->all() as $error)
+                        <li class="list-group-item text-danger">
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{route('categories.store')}}" method="post">
+            @csrf
             <div class="form-group">
                 <label for="name">Name</label>
                 <input type="text" id="name" class="form-control" name="name">
