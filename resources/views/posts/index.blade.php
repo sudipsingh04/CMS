48ac22ad66bc5c5469c6dbbbdb172e09eca11d28
Sudip Kumar Singh <sudip.singh@capitalnumbers.com>
2020-08-09 14:39:35 +0530

    Display posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,25 @@
 <div class="card card-default">
     <div class="card-header">Posts</div>
     <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Actions</th>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td><img src="{{ asset('storage/'.$post->image) }}" width="60px" height="60px"></td>
+                        <td>{{ $post->title }}</td>
+                        <td>
+                            <a href="" class="btn btn-info btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Trash</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
