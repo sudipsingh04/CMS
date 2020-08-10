4e0e049cce4d479c83f8c4ce3ce7c2ddaa0d7af9
sudipsingh04 <sudipsingh04@gmail.com>
2020-08-10 17:20:16 +0530

    Fixed tag and category delete

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -94,6 +94,12 @@ class CategoriesController extends Controller
      */
     public function destroy(Category $category)
     {
+        if ($category->posts->count() > 0) {
+            session()->flash('error', 'Category cannot be deleted as it contains some post');
+
+            return redirect()->back();
+        }
+
         $category->delete();
 
         session()->flash('success', 'Category deleted successfully');
--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -94,6 +94,12 @@ class TagsController extends Controller
      */
     public function destroy(Tag $tag)
     {
+        if ($tag->posts->count() > 0) {
+            session()->flash('error', 'Tag cannot be deleted as it contains some post');
+
+            return redirect()->back();
+        }
+
         $tag->delete();
 
         session()->flash('success', 'Tag deleted successfully');
