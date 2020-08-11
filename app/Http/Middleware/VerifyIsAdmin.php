79eb9ada15d05ce697c971903a86206c48551aa5
sudipsingh04 <sudipsingh04@gmail.com>
2020-08-11 11:40:00 +0530

    Fixed verifyAdmin middleware

--- app/Http/Middleware/VerifyIsAdmin.php
+++ app/Http/Middleware/VerifyIsAdmin.php
@@ -15,7 +15,7 @@ class VerifyIsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if(!auth()->user()->isAdmin){
+        if(!auth()->user()->isAdmin()){
             return redirect()->route('home');
         }
         return $next($request);
