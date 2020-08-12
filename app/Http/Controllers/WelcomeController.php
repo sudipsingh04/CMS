857c8b38c35e13c48f6ebc23563a13b792ecae33
sudipsingh04 <sudipsingh04@gmail.com>
2020-08-12 17:51:30 +0530

    Search functionality

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,9 +11,17 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        $search = request()->query('search');
+
+        if($search){
+            $posts = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(4);
+        }else{
+            $posts = Post::simplePaginate(4);
+        }
+
         return view('welcome')
             ->with('categories', Category::all())
             ->with('tags', Tag::all())
-            ->with('posts', Post::simplePaginate(4));
+            ->with('posts', $posts);
     }
 }
